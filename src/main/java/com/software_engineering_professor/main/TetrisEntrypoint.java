d0c4212548c276dfed0d88b39e95e582f2419265
javier <javier.mediavilla.vegas@code2run.net>
2020-09-05 22:53:55 +0200

    After 84.

--- src/main/java/com/software_engineering_professor/main/TetrisEntrypoint.java
+++ src/main/java/com/software_engineering_professor/main/TetrisEntrypoint.java
@@ -7,21 +7,26 @@ import java.io.IOException;
 public class TetrisEntrypoint {
     private static final int DEFAULT_BOARD_SIZE = 30;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        int width = DEFAULT_BOARD_SIZE;
-        int height = DEFAULT_BOARD_SIZE;
+    private int width = DEFAULT_BOARD_SIZE;
+    private int height = DEFAULT_BOARD_SIZE;
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TetrisEntrypoint entrypoint = new TetrisEntrypoint();
+        entrypoint.parseParams(args);
+        new TetrisPackage(entrypoint.width, entrypoint.height).start();
+    }
+
+    private void parseParams(String[] args) {
         if(args.length == 2) {
             width = Integer.valueOf(args[0]);
             height = Integer.valueOf(args[1]);
         } else if(args.length == 1 || args.length > 2) {
-            TetrisEntrypoint.printHelp();
+            printHelp();
             System.exit(-1);
         }
-        new TetrisPackage(width, height).start();
     }
 
-    private static void printHelp() {
+    private void printHelp() {
         System.out.println("You are using the program wrong. The right ways are:");
         System.out.println(" * tetris");
         System.out.println(" * tetris <width> <height>");
