30bb59d59222c22f48cbdbf7cdc05a3cb1385ac7
javier <javier.mediavilla.vegas@code2run.net>
2020-09-05 14:34:40 +0200

    After 74.

--- src/main/java/com/software_engineering_professor/graphics/lanterna/controller/InputController.java
+++ src/main/java/com/software_engineering_professor/graphics/lanterna/controller/InputController.java
@@ -1,12 +1,20 @@
 package com.software_engineering_professor.graphics.lanterna.controller;
 
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.software_engineering_professor.engine.controller.Controller;
+import com.software_engineering_professor.engine.event.Event;
 import com.software_engineering_professor.engine.event.EventQueue;
+import com.software_engineering_professor.engine.event.EventType;
 
 import java.io.IOException;
 import java.util.Objects;
+
+import static com.googlecode.lanterna.input.KeyType.*;
+import static com.software_engineering_professor.engine.event.EventType.MOVE_DOWN;
+import static com.software_engineering_professor.engine.event.EventType.MOVE_HORIZONTAL;
+import static com.software_engineering_professor.engine.event.EventType.ROTATE_LEFT;
 
 public class InputController implements Controller {
     private static final int MAX_NUMBER_OF_EVENTS_PER_CALL = 10;
@@ -34,10 +42,35 @@ public class InputController implements Controller {
         for(int i = 0; i < MAX_NUMBER_OF_EVENTS_PER_CALL; i++) {
             try {
                 KeyStroke keyStroke = terminal.pollInput();
-                eventQueue.addEvent(getEvent(keyStroke.getCharacter()));
+                Event event = getEvent(keyStroke);
+                if(event != null) {
+                    eventQueue.addEvent(event);
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    private Event getEvent(KeyStroke keyStroke) {
+        if(keyStroke == null) {
+            return null;
+        } else {
+            return getEvent(keyStroke.getKeyType());
+        }
+    }
+
+    private Event getEvent(KeyType keyType) {
+        switch (keyType) {
+            case ArrowLeft:
+                return new Event(MOVE_HORIZONTAL, -1);
+            case ArrowRight:
+                return new Event(MOVE_HORIZONTAL, 1);
+            case ArrowDown:
+                return new Event(MOVE_DOWN, 1);
+            case ArrowUp:
+                return new Event(ROTATE_LEFT, 1);
+            default:
+                return null;
         }
     }
 }
