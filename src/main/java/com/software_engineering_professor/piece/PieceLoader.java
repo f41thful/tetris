9184eea88bceb9aa192ae90665a3e90c6ebdf536
javier <javier.mediavilla.vegas@code2run.net>
2020-09-05 10:15:10 +0200

    After 56.

--- src/main/java/com/software_engineering_professor/piece/PieceLoader.java
+++ src/main/java/com/software_engineering_professor/piece/PieceLoader.java
@@ -1,13 +1,17 @@
 package com.software_engineering_professor.piece;
 
+import com.software_engineering_professor.board.PositionValidation;
+
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Objects;
 
 public class PieceLoader {
-    private PieceBuilder builder;
+    private PositionValidation positionValidation;
 
-    public PieceLoader() {
-        builder = new PieceBuilder();
+    public PieceLoader(PositionValidation positionValidation) {
+        Objects.requireNonNull(positionValidation);
+        this.positionValidation = positionValidation;
     }
 
     public Collection<Piece> getDefaultPieces() {
@@ -23,36 +27,40 @@ public class PieceLoader {
     }
 
     private Piece piece0() {
-        return builder.add("xxxx").build();
+        return getBuilder(0).add("xxxx").build();
     }
 
     private Piece piece1() {
-        return builder.add("x")
-                      .add("xxx").build();
+        return getBuilder(1).add("x")
+                            .add("xxx").build();
     }
 
     private Piece piece2() {
-        return builder.add("  x")
-                      .add("xxx").build();
+        return getBuilder(2).add("  x")
+                            .add("xxx").build();
     }
 
     private Piece piece3() {
-        return builder.add("xx")
-                      .add("xx").build();
+        return getBuilder(3).add("xx")
+                            .add("xx").build();
     }
 
     private Piece piece4() {
-        return builder.add(" xx")
-                      .add("xx ").build();
+        return getBuilder(4).add(" xx")
+                            .add("xx ").build();
     }
 
     private Piece piece5() {
-        return builder.add(" x ")
-                      .add("xxx").build();
+        return getBuilder(5).add(" x ")
+                            .add("xxx").build();
     }
 
     private Piece piece6() {
-        return builder.add("xx ")
-                      .add(" xx").build();
+        return getBuilder(6).add("xx ")
+                            .add(" xx").build();
+    }
+
+    private PieceBuilder getBuilder(int type) {
+        return PieceBuilder.create(type).positionValidation(positionValidation);
     }
 }
