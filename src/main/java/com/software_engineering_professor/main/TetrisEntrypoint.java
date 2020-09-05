bbc1a0a9ba6eecddb2a3ec75a246203d14968cc3
javier <javier.mediavilla.vegas@code2run.net>
2020-09-05 12:12:06 +0200

    After 67.

---
+++ src/main/java/com/software_engineering_professor/main/TetrisEntrypoint.java
@@ -0,0 +1,11 @@
+package com.software_engineering_professor.main;
+
+import com.software_engineering_professor.TetrisPackage;
+
+import java.io.IOException;
+
+public class TetrisEntrypoint {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new TetrisPackage(40, 20).start();
+    }
+}
