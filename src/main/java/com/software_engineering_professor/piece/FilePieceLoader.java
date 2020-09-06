4b7d62f5eaef4c7edd155409b1aa11cd411b23aa
javier <javier.mediavilla.vegas@code2run.net>
2020-09-06 16:19:45 +0200

    After 92.

---
+++ src/main/java/com/software_engineering_professor/piece/FilePieceLoader.java
@@ -0,0 +1,93 @@
+package com.software_engineering_professor.piece;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+public class FilePieceLoader {
+    private static final String DEFAULT_PATH = "/extra-pieces/";
+
+    private final String path;
+
+    public FilePieceLoader() {
+        this(DEFAULT_PATH);
+    }
+
+    public FilePieceLoader(String path) {
+        Objects.requireNonNull(path);
+        this.path = path;
+    }
+
+    public Collection<Piece> getPieces(int nextType) {
+        if(nextType < 0) {
+            throw new IllegalArgumentException("Type must be >= 0. It was " + nextType + ".");
+        }
+
+        try {
+            List<String> fileNames = getPieceFileNames();
+            return createPieces(nextType, fileNames);
+        } catch(Exception e) {
+            System.out.println("Error trying to read " + path + ". Just skip it.");
+        }
+
+        return new ArrayList<>();
+    }
+
+    private Collection<Piece> createPieces(int nextType, Collection<String> fileNames) throws IOException {
+        Collection<Piece> pieces = new ArrayList<>();
+        for(String fileName : fileNames) {
+            if(fileName == null) {continue;}
+
+            pieces.add(createPiece(nextType, fileName));
+            nextType++;
+        }
+
+        return pieces;
+    }
+
+    private Piece createPiece(int nextType, String fileName) throws IOException {
+        List<String> lines = Files.readAllLines(absPath(fileName));
+        PieceBuilder builder = PieceBuilder.create(nextType);
+        lines.stream().forEach(builder::add);
+
+        return builder.build();
+    }
+
+    private Path absPath(String fileName) {
+        return Paths.get(path + fileName);
+    }
+
+    private List<String> getPieceFileNames() throws IOException {
+        List<String> filenames = new ArrayList<>();
+
+        InputStream in = getResourceAsStream(path);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        String resource;
+
+        while ((resource = br.readLine()) != null) {
+            filenames.add(resource);
+        }
+
+        return filenames;
+    }
+
+    private InputStream getResourceAsStream(String resource) {
+        final InputStream in
+                = getContextClassLoader().getResourceAsStream(resource);
+
+        return in == null ? getClass().getResourceAsStream(resource) : in;
+    }
+
+    private ClassLoader getContextClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+}
